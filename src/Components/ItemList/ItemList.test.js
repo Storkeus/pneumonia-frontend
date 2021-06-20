ac890c492f22c703e7bcf5553c16dca9090de1f9
Bartosz Łyżwa <bartosz.lyzwa@o2.pl>
2021-06-20 13:20:55 +0200

    Fix item list rendering error with no head or rows provided
    Added default value for head and rows prop (empty array)

--- src/Components/ItemList/ItemList.test.js
+++ src/Components/ItemList/ItemList.test.js
@@ -1,7 +1,4 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
-import ReactTestUtils from 'react-dom/test-utils';
-
 import ItemListTable from './ItemListTable';
 import ItemList from './ItemList';
 
@@ -18,45 +15,3 @@ import ItemList from './ItemList';
 it('ItemList renders without crashing',()=>{
   render(<ItemList/>);
 });
-
-
-
-
-// test("should show error message on non valid e-mail", () => {
-//   render(
-//     <MemoryRouter initialEntries={["/login"]}>
-//       <Login />
-//     </MemoryRouter>
-//   );
-//   const eMailInput = screen.getByLabelText("Adres e-mail:");
-//   fireEvent.change(eMailInput, {
-//     target: { value: "not a valid e-mail" },
-//   });
-
-
-//   const button = document.querySelector('button');
-//   fireEvent.click(button);
-
-//   const validationError = screen.getByText(/Adres e-mail jest nieprawidłowy/i);
-//   expect(validationError).toBeInTheDocument();
-// });
-
-
-// test("should pass mail with diactric characters", () => {
-//   render(
-//     <MemoryRouter initialEntries={["/login"]}>
-//       <Login />
-//     </MemoryRouter>
-//   );
-//   const eMailInput = screen.getByLabelText("Adres e-mail:");
-//   fireEvent.change(eMailInput, {
-//     target: { value: "żółć@żółć.com" },
-//   });
-
-  
-//   const button = document.querySelector('button');
-//   fireEvent.click(button);
-
-//   const validationError = screen.queryByText(/Adres e-mail jest nieprawidłowy/i);
-//   expect(validationError).toBeNull();
-// });
