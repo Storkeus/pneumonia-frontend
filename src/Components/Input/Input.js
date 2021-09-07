0c1a9bb677ab343a929fab217bed262cd94e66ee
Bartosz Łyżwa <bartosz.lyzwa@o2.pl>
2021-09-07 19:10:51 +0200

    Bring back input component

---
+++ src/Components/Input/Input.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+/**
+ * Input component
+ * @param {object} props
+ * @param {null|string|number} props.value value of input if given from parent
+ * @param {null|function} props.onChange change event handler if given from parent
+ * @param {string} props.name input name
+ * @param {string} props.type input type
+ * @param {string} props.errorInfo validation error info
+ * @returns
+ */
+const Input = (props) => {
+    const [inputValueHook, setInputValueHook] = useState("");
+
+    const inputValue = props.value ? props.value : inputValueHook;
+    const setInputValue = props.onChange ? props.onChange : setInputValueHook;
+
+    const { innerRef = null, accept = null } = props;
+
+    return (
+        <input
+            className={props.className}
+            name={props.name}
+            id={props.name}
+            readOnly={props.readOnly}
+            ref={innerRef}
+            accept={accept}
+            value={inputValue}
+            type={props.type ? props.type : "text"}
+            onChange={(e) => {
+                if (props.type && props.type === "file") {
+                    setInputValue(e);
+                } else {
+                    setInputValue(e.target.value);
+                }
+            }}
+        />
+    );
+};
+export default Input;
---
+++ src/Components/Input/Input.test.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import Input from "./Input";
+
+describe("Input", () => {
+    /**
+     * Smoke tests of ItemList Components
+     */
+
+    test("Input renders without crashing", () => {
+        render(<Input />);
+    });
+});
